96283c9ba82b12ff15fb74e073f3b9fa01d9748e
vandyG <vandy.goel23@gmail.com>
2023-02-05 20:36:11 +0530

    remove commands.yaml and update test_clickyaml
    remove test file and add string fixture instead

--- tests/test_clickyaml.py
+++ tests/test_clickyaml.py
@@ -9,13 +9,41 @@ from click.testing import CliRunner
 from clickyaml import commander
 
 @pytest.fixture
-def cmdr():
+def yaml_str():
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
+    yaml_str = """
+    commands:
+        adhoc:
+            script: "nohup /home/user/script/source/run_adhoc.scr"
+            help: "Invokes an adhoc run for SAS ABPs. Requires three parameters ABP ID, Analytic Type and LOB/Platform. ABP ID should just be without the 'abp' prefix."
+            params:
+                - !arg
+                    param_decls: [id]
+                - !arg
+                    param_decls: [type]
+                    type: !obj
+                        class: click.Choice
+                        choices: ["a", "b"]
+                        case_sensitive: False
+                - !arg
+                    param_decls: [category]
+                    type: !obj
+                        class: click.Choice
+                        choices: ["1","2","3","ALL"]
+                        case_sensitive: False
+                - !opt
+                    param_decls: ["--email","-E"]
+                    multiple: True
+                    envvar: MY_EMAIL
+                    help: "Specify the mailing list with this option"
+    """
 
-    return commander.Commander.create_commander('/mnt/c/Users/vgoel9/OneDrive - UHG/Rules/Special/clickyaml/tests/commands.yaml')
+    return yaml_str
+
+    # return commander.Commander.create_commander('/mnt/c/Users/vgoel9/OneDrive - UHG/Rules/Special/clickyaml/tests/commands.yaml')
 
 
 
@@ -25,11 +53,13 @@ def test_content():
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
-def test_parse_yaml(cmdr):
+def test_parse_yaml(yaml_str):
     """Test parse_pyaml"""
     # parsed = clickyaml.parse_yaml('/mnt/c/Users/vgoel9/OneDrive - UHG/Rules/Special/clickyaml/tests/commands.yaml')
     # assert "commands" in parsed.keys()
     # assert isinstance(parsed["commands"], dict)
+
+    cmdr = commander.Commander.create_commander(data=yaml_str)
 
     assert all(isinstance(key, str) for key in cmdr.parsed_yaml.keys())
     assert all(isinstance(value, dict) for value in cmdr.parsed_yaml.values())
@@ -38,13 +68,15 @@ def test_parse_yaml(cmdr):
     assert "help" in cmdr.parsed_yaml["adhoc"]
     assert "params" in cmdr.parsed_yaml["adhoc"]
 
-def test_get_command(cmdr):
+def test_get_command(yaml_str):
 
-    command, script = cmdr.get_command("adhoc", lambda abp, analytictype, lobplat, email, runtype : print("hello"))
+    cmdr = commander.Commander.create_commander(data=yaml_str)
+
+    command, script = cmdr.get_command("adhoc", lambda id, type, category, email : print("hello"))
     assert "adhoc" == command.name
     assert "run_adhoc.scr" in script
 
     runner = CliRunner()
-    result = runner.invoke(command, ["4402", "prof","unet"])
+    result = runner.invoke(command, ["4402", "a","all"])
     assert "hello" in result.output
     assert result.exit_code == 0
