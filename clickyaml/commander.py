a82d55f2eaf2914257e093f4c8d2fc3a7d7b45ea
vandyG <vandy.goel23@gmail.com>
2023-02-15 16:51:30 +0530

    add properties for callback and command

--- clickyaml/commander.py
+++ clickyaml/commander.py
@@ -3,7 +3,6 @@ from subprocess import Popen
 from typing import Any
 from typing import Callable
 import click
-from re import split
 
 
 @dataclass()
@@ -17,26 +16,28 @@ class Commander:
     :type name: str
     :param parsed_yaml: Dictionary of commands, can include one or more commands.
     :type parsed_yaml: dict
-    :param callback: Callback to be linked to the click Command.
-    :type callback: Callable
     """
 
     name: str
     parsed_yaml: dict
-    callback: Callable | None = field(repr=False,default=None)
     script: str = field(init=False, default="")
-    _command: click.Command = field(init=False,repr=False)
+    _callback: Callable | None = field(repr=False, default=None, init=False)
+    _command: click.Command = field(init=False, repr=False)
 
     def __post_init__(self) -> None:
-        self.parsed_yaml = self.parsed_yaml[self.name]
-        self.script = self.parsed_yaml.pop("script") if "script" in self.parsed_yaml else ""
-        self.callback = self.__default_callback__ if not self.callback else self.callback
-        self._command = click.Command(name=self.name, callback=self.callback, **self.parsed_yaml)
+        self.script = (
+            self.parsed_yaml.pop("script") if "script" in self.parsed_yaml else ""
+        )
+        self._callback = (
+            self.__default_callback__ if not self._callback else self._callback
+        )
+        self._command = click.Command(
+            name=self.name, callback=self._callback, **self.parsed_yaml
+        )
 
     def __default_callback__(self, **kwargs) -> None:
-        """The default callback assigned to the click command.
-        """
-        script_parms = split(r"(?<!\\)\s", self.script)
+        """The default callback assigned to the click command."""
+        script_parms = self.script.split()
         params_in_order = [kwargs[key.lower()] for key in self.parsed_yaml["params"]]
 
         Popen(script_parms + params_in_order, text=True)
@@ -49,5 +50,24 @@ class Commander:
         :return: Click command created from the yaml file.
         :rtype: class: click.Command
         """
-        self._command = click.Command(name=self.name, callback=self.callback, **self.parsed_yaml)
+        self._command = click.Command(
+            name=self.name, callback=self._callback, **self.parsed_yaml
+        )
         return self._command
+
+    @property
+    def callback(self):
+        """Callback to be linked to the click Command.
+
+        :return: The callback linked to the click command
+        :rtype: Callable
+        """
+        return self._callback
+
+    @callback.setter
+    def callback(self, value):
+        if callable(value):
+            self._callback = value
+            self.command.callback = value
+        else:
+            raise TypeError("'value' needs to be a function/lambda")
