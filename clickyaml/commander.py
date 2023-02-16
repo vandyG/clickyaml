59cbf0880ff644897f950c851eba792b270b9826
vandyG <vandy.goel23@gmail.com>
2023-02-16 17:35:14 +0530

    fix type hint bug

--- clickyaml/commander.py
+++ clickyaml/commander.py
@@ -21,7 +21,7 @@ class Commander:
     name: str
     parsed_yaml: dict
     script: str = field(init=False, default="")
-    _callback = field(repr=False, default=None, init=False)
+    _callback: Any = field(repr=False, default=None, init=False)
     _command: click.Command = field(init=False, repr=False)
 
     def __post_init__(self) -> None:
