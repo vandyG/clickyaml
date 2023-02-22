c61ece31ccef5dab9934112948c3a47edb9be832
vandyG <vandy.goel23@gmail.com>
2023-02-22 21:10:19 +0530

    Bump version: 1.1.2 → 2.0.0

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/vandyG/clickyaml',
-    version='1.1.2',
+    version='2.0.0',
     zip_safe=False,
 )
