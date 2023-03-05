cf08cd8d3708d278aa0922ab42051f84420af65c
vandyG <vandy.goel23@gmail.com>
2023-03-05 21:53:23 +0530

    Bump version: 2.0.0 → 2.1.0

--- clickyaml/__init__.py
+++ clickyaml/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Vandit Goel"""
 __email__ = "vandy.goel23@gmail.com"
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 from .clickyaml import parse_yaml, get_command, get_commanders
